4d1fb22bc3b057a70ff0107c6399e7b6dc12f7af
Macarena Oyague <macarena.oyague@utec.edu.pe>
2021-07-26 14:25:52 -0500

    setup

---
+++ code/src/main/java/cs/lab/Main.java
@@ -0,0 +1,12 @@
+package cs.lab;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+    }
+
+}
---
+++ code/src/test/java/cs/lab/Tester.java
@@ -0,0 +1,16 @@
+package cs.lab;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Scanner;
+
+class Tester {
+
+    @Test
+    public void test() throws Exception {
+
+        Assert.assertEquals(persona.comprarLibro("Cien años","Vargas");
+    }
+
+};
